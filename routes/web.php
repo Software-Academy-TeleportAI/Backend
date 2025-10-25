034d5efe1014a80bb4a97bb9c9d9a00e7ae87db3
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2025-10-25 16:23:50 +0300

    create controller, UI page and routing for Project details UI

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PlaygroundController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -16,6 +17,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/playground', [PlaygroundController::class, 'index'])->name('playground.index');
     Route::post('/playground', [PlaygroundController::class, 'store'])->name('playground.store');
+    Route::get('/project/{owner}/{repo}', [ProjectController::class, 'show'])->name('project.show');
 });
 
 require __DIR__.'/settings.php';
