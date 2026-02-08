281523ab479f0d967306a1e04dc0cd2b9312acc2
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2026-02-08 16:12:13 +0200

    re-run migartions

--- database/migrations/2026_02_08_105910_create_repository_analyses_table.php
+++ database/migrations/2026_02_08_105910_create_repository_analyses_table.php
@@ -10,12 +10,20 @@ return new class extends Migration
     {
         Schema::create('repository_analyses', function (Blueprint $table) {
             $table->id();
+
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            // 2. Repository Info
             $table->unsignedBigInteger('repository_id')->index();
+            $table->string('repo_name')->nullable(); // Don't forget this!
+
+            // 3. Analysis Content
             $table->text('summary')->nullable();
             $table->text('architecture_diagram')->nullable();
             $table->mediumText('readme')->nullable();
             $table->json('files')->nullable();
-            
+
             $table->timestamps();
         });
     }
