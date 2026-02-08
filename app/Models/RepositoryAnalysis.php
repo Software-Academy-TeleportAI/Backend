cf9c009d5f7d9b7e7f25cb96306181e738ab9dd6
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2026-02-08 15:22:22 +0200

    create RepositoryAnalysis model

---
+++ app/Models/RepositoryAnalysis.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RepositoryAnalysis extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'repository_id',
+        'repo_name',
+        'summary',
+        'architecture_diagram',
+        'readme',
+        'files',
+    ];
+
+    protected $casts = [
+        'files' => 'array',
+    ];
+}
